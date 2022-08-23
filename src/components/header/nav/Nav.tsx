4c9e986888edc86390ef9b7f52a5cc39dee208b6
ToshaBY <anton.rybakou@gmail.com>
2022-08-23 23:33:25 +0300

    remove: Resume in navigation

--- src/components/header/nav/Nav.tsx
+++ src/components/header/nav/Nav.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import style from './Nav.module.css';
 import {Link} from 'react-scroll';
-import CV from '../../../assets/files/CV.pdf'
 
 export const Nav = React.memo(() => {
     return (
@@ -9,22 +8,15 @@ export const Nav = React.memo(() => {
             <Link to='main' spy={true} smooth={true} offset={-70} duration={500}>
                 Main
             </Link>
-
             <Link to='skills' spy={true} smooth={true} offset={-70} duration={500}>
                 Skills
             </Link>
-
             <Link to='projects' spy={true} smooth={true} offset={-70} duration={500}>
                 Projects
             </Link>
-
             <Link to='contacts' spy={true} smooth={true} offset={-70} duration={500}>
                 Contacts
             </Link>
-
-            <a href={CV} target='_blank' rel={'noreferrer'} download={'Anton Rybakou CV.pdf'}>
-                Resume
-            </a>
         </nav>
     )
 })
