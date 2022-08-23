39507dc8cd3dddad9c536fc2ca8a4da9bbb48334
ToshaBY <anton.rybakou@gmail.com>
2022-08-23 23:46:26 +0300

    add: resume link

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from '../../common/styles/Container.module.scss';
+// import styleButton from "../../common/styles/Buttons.module.scss";
+import CV from '../../assets/files/CV.pdf';
 import photo from '../../assets/images/photo.jpg'
 import {ParticlesComp} from "./ParticlesComp";
 
@@ -13,6 +15,10 @@ export const Main = React.memo(() => {
                     <span>Hello there,</span>
                     <h1>I'm Anton Rybakou</h1>
                     <p>Front-end developer</p>
+                    <a href={CV} target='_blank' rel={'noreferrer'}
+                       download={'Anton Rybakou CV.pdf'}>
+                        Download my CV
+                    </a>
                 </div>
                 <div className={style.photo}>
                     <img src={photo} alt={'myPhoto'}/>
