c8f64dc2bb7a71c32e55834cf688257b07dbe718
ToshaBY <anton.rybakou@gmail.com>
2022-09-13 23:44:36 +0300

    add: arhi portfolio project

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import style from './Footer.module.scss';
-import styleContainer from "../../common/styles/Container.module.scss";
-import {Title} from "../../common/components/title/Title";
-import {faGithub, faLinkedin, faTelegram} from "@fortawesome/free-brands-svg-icons";
-import {SocialItem} from "./socialItem/SocialItem";
-import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import styleContainer from '../../common/styles/Container.module.scss';
+import { Title } from '../../common/components/title/Title';
+import { faGithub, faLinkedin, faTelegram } from '@fortawesome/free-brands-svg-icons';
+import { SocialItem } from './socialItem/SocialItem';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 export const Footer = React.memo(() => {
-    const socialLinks = [
-        {id: 1, icon: faGithub, link: 'https://github.com/ToshaBY'},
-        {id: 2, icon: faLinkedin, link: 'https://www.linkedin.com/in/antonrybakov/'},
-        {id: 3, icon: faTelegram, link: 'https://t.me/untosha'},
-        {id: 4, icon: faEnvelope, link: 'mailto:anton.rybakou@gmail.com'},
-    ]
+  const socialLinks = [
+    { id: 1, icon: faGithub, link: 'https://github.com/ToshaBY' },
+    { id: 2, icon: faLinkedin, link: 'https://www.linkedin.com/in/antonrybakov/' },
+    { id: 3, icon: faTelegram, link: 'https://t.me/untosha' },
+    { id: 4, icon: faEnvelope, link: 'mailto:anton.rybakou@gmail.com' },
+  ];
 
-    return (
-        <div className={`${styleContainer.block} ${style.footerBlock}`}>
-            <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <Title title={'Anton Rybakou'}/>
-                <div className={style.socialBlock}>
-                    {socialLinks.map(el => {
-                        return (
-                            <SocialItem key={el.id}
-                                        icon={el.icon}
-                                        link={el.link}/>
-                        )
-                    })}
-                </div>
-                <div className={style.copyright}>
-                    © 2022 All rights reserved
-                </div>
-            </div>
+  return (
+    <div className={`${styleContainer.block} ${style.footerBlock}`}>
+      <div className={`${styleContainer.container} ${style.footerContainer}`}>
+        <Title title={'Anton Rybakou'} />
+        <div className={style.socialBlock}>
+          {socialLinks.map(el => {
+            return (
+              <SocialItem key={el.id}
+                          icon={el.icon}
+                          link={el.link} />
+            );
+          })}
         </div>
-    );
-})
+        <div className={style.copyright}>
+          © 2022 All rights reserved
+        </div>
+      </div>
+    </div>
+  );
+});
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,72 +1,81 @@
 import React from 'react';
 import style from './Projects.module.scss';
 import styleContainer from '../../common/styles/Container.module.scss';
-import {Project} from './project/Project';
-import {Title} from '../../common/components/title/Title';
+import { Project } from './project/Project';
+import { Title } from '../../common/components/title/Title';
 import counterImage from '../../assets/images/projects/counter.png';
 import todoImage from '../../assets/images/projects/todolist.png';
 import socialImage from '../../assets/images/projects/social-network.png';
 import cardsImage from '../../assets/images/projects/cards.png';
 
 export const Projects = React.memo(() => {
-    const projects = [
-        {
-            id: 1,
-            title: 'Learning cards',
-            description: 'Team work / GitHub / TypeScript / React / Redux-Toolkit / Formik / React-router-dom / Material-ui / Deploy (In progress)',
-            url: 'https://alexnewhere.github.io/friday-project/',
-            style: {
-                backgroundImage: `url(${cardsImage})`
-            }
-        },
-        {
-            id: 2,
-            title: 'Social network',
-            description: 'TypeScript / React / MUI / Storybook / Redux / Redux-Thunk / Axios / Formik / React-router-dom / Unit tests / Deploy (In progress)',
-            url: 'https://github.com/ToshaBY/it-incubator-samurai-way',
-            style: {
-                backgroundImage: `url(${socialImage})`
-            }
-        },
-        {
-            id: 3,
-            title: 'To-Do List',
-            description: 'TypeScript / React / MUI / Storybook / Redux / Redux-Thunk / Axios / Formik / React-router-dom / Unit tests / Deploy',
-            url: 'https://toshaby.github.io/todo-list/',
-            style: {
-                backgroundImage: `url(${todoImage})`
-            }
-        },
-        {
-            id: 4,
-            title: 'Counter',
-            description: 'TypeScript / React / Redux',
-            url: 'https://toshaby.github.io/counter/',
-            style: {
-                backgroundImage: `url(${counterImage})`
-            }
-        },
-    ]
+  const projects = [
+    {
+      id: 1,
+      title: 'Learning cards',
+      description: 'Team work / GitHub / TypeScript / React / Redux-Toolkit / Formik / React-router-dom / Material-ui / Deploy (In progress)',
+      url: 'https://alexnewhere.github.io/friday-project/',
+      style: {
+        backgroundImage: `url(${cardsImage})`,
+      },
+    },
+    {
+      id: 2,
+      title: 'Social network',
+      description: 'TypeScript / React / MUI / Storybook / Redux / Redux-Thunk / Axios / Formik / React-router-dom / Unit tests / Deploy (In progress)',
+      url: 'https://github.com/ToshaBY/it-incubator-samurai-way',
+      style: {
+        backgroundImage: `url(${socialImage})`,
+      },
+    },
+    {
+      id: 3,
+      title: 'To-Do List',
+      description: 'TypeScript / React / MUI / Storybook / Redux / Redux-Thunk / Axios / Formik / React-router-dom / Unit tests / Deploy',
+      url: 'https://toshaby.github.io/todo-list/',
+      style: {
+        backgroundImage: `url(${todoImage})`,
+      },
+    },
+    {
+      id: 4,
+      title: 'Counter',
+      description: 'TypeScript / React / Redux',
+      url: 'https://toshaby.github.io/counter/',
+      style: {
+        backgroundImage: `url(${counterImage})`,
+      },
+    },
+    {
+      id: 5,
+      title: 'Friend\'s portfolio',
+      description: 'React / React-router / MUI / emailjs',
+      url: 'https://arhifranki.github.io/portfolio',
+      style: {
+        backgroundImage: `url(${socialImage})`,
+      },
+    },
+  ];
 
-    return (
-        <div className={`${styleContainer.block} ${style.projectsBlock}`} id={'projects'}>
-            <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <Title title="Projects"/>
+  return (
+    <div className={`${styleContainer.block} ${style.projectsBlock}`} id={'projects'}>
+      <div className={`${styleContainer.container} ${style.projectsContainer}`}>
+        <Title title='Projects' />
 
-                <div className={styleContainer.contentContainer}>
-                    {
-                        projects.map(el => {
-                            return (
-                                <Project key={el.id}
-                                         title={el.title}
-                                         description={el.description}
-                                         url={el.url}
-                                         style={el.style}/>
-                            )
-                        })
-                    }
-                </div>
-            </div>
+        <div className={styleContainer.contentContainer}>
+          {
+            projects.map(el => {
+              return (
+                <Project key={el.id}
+                         title={el.title}
+                         description={el.description}
+                         url={el.url}
+                         style={el.style} />
+              );
+            })
+          }
         </div>
-    );
-})
+      </div>
+    </div>
+  );
+});
