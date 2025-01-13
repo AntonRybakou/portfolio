23bd0884f1f388d6bb3950ba8ee21a1f78d75874
Anton Rybakou <anton.rybakou@gmail.com>
2025-01-13 20:49:24 +0300

    feat: footer social links

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,6 @@
 import { ThemeSwitcher } from '@/components/layout/theme-switcher'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export const Footer = () => {
   return (
@@ -7,6 +9,63 @@ export const Footer = () => {
         <p className="text-sm text-muted-foreground">
           © {new Date().getFullYear()} Anton Rybakou
         </p>
+        <div className="flex flex-row items gap-2">
+          <Link
+            href="https://github.com/AntonRybakou/"
+            className="text-sm text-muted-foreground"
+            target="_blank"
+          >
+            <Image
+              src="/icons/github.svg"
+              alt="Github"
+              width={24}
+              height={24}
+              className="dark:invert hover:brightness-0 hover:invert-[.45] hover:sepia hover:saturate-[600%] hover:hue-rotate-[350deg] transition-all duration-300"
+            />
+          </Link>
+
+          <Link
+            href="https://linkedin.com/in/antonrybakov"
+            className="text-sm text-muted-foreground"
+            target="_blank"
+          >
+            <Image
+              src="/icons/linkedin.svg"
+              alt="LinkedIn"
+              width={24}
+              height={24}
+              className="dark:invert hover:brightness-0 hover:invert-[.45] hover:sepia hover:saturate-[600%] hover:hue-rotate-[350deg] transition-all duration-300"
+            />
+          </Link>
+
+          <Link
+            href="https://t.me/untosha"
+            className="text-sm text-muted-foreground"
+            target="_blank"
+          >
+            <Image
+              src="/icons/telegram.svg"
+              alt="Telegram"
+              width={24}
+              height={24}
+              className="dark:invert hover:brightness-0 hover:invert-[.45] hover:sepia hover:saturate-[600%] hover:hue-rotate-[350deg] transition-all duration-300"
+            />
+          </Link>
+
+          <Link
+            href="mailto:anton.rybakou@gmail.com"
+            className="text-sm text-muted-foreground"
+            target="_blank"
+          >
+            <Image
+              src="/icons/gmail.svg"
+              alt="Gmail"
+              width={24}
+              height={24}
+              className="dark:invert hover:brightness-0 hover:invert-[.45] hover:sepia hover:saturate-[600%] hover:hue-rotate-[350deg] transition-all duration-300"
+            />
+          </Link>
+        </div>
         <ThemeSwitcher />
       </div>
     </footer>
