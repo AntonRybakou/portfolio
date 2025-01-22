1e470424a0a5b6c73b1cc30a23159c42db12beae
Anton Rybakou <anton.rybakou@gmail.com>
2025-01-22 20:38:20 +0300

    chore: project update, remove link hovers

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image'
 
 export const Footer = () => {
   return (
-    <footer className="w-full h-14 my-4">
+    <footer className="w-full h-14 my-4 z-10">
       <div className="max-w-screen-lg mx-auto flex h-14 items-center justify-between px-4 lg:px-0">
         <p className="text-sm text-muted-foreground">
           © {new Date().getFullYear()} Anton Rybakou
         </p>
-        <div className="flex flex-row items gap-2">
+        <div className="flex flex-row items gap-4">
           <Link
             href="https://github.com/AntonRybakou/"
             className="text-sm text-muted-foreground"
@@ -20,7 +20,7 @@ export const Footer = () => {
               alt="Github"
               width={24}
               height={24}
-              className="dark:invert hover:brightness-0 hover:invert-[.45] hover:sepia hover:saturate-[600%] hover:hue-rotate-[350deg] transition-all duration-300"
+              className="dark:invert"
             />
           </Link>
 
@@ -34,7 +34,7 @@ export const Footer = () => {
               alt="LinkedIn"
               width={24}
               height={24}
-              className="dark:invert hover:brightness-0 hover:invert-[.45] hover:sepia hover:saturate-[600%] hover:hue-rotate-[350deg] transition-all duration-300"
+              className="dark:invert"
             />
           </Link>
 
@@ -48,7 +48,7 @@ export const Footer = () => {
               alt="Telegram"
               width={24}
               height={24}
-              className="dark:invert hover:brightness-0 hover:invert-[.45] hover:sepia hover:saturate-[600%] hover:hue-rotate-[350deg] transition-all duration-300"
+              className="dark:invert"
             />
           </Link>
 
@@ -62,7 +62,7 @@ export const Footer = () => {
               alt="Gmail"
               width={24}
               height={24}
-              className="dark:invert hover:brightness-0 hover:invert-[.45] hover:sepia hover:saturate-[600%] hover:hue-rotate-[350deg] transition-all duration-300"
+              className="dark:invert"
             />
           </Link>
         </div>
